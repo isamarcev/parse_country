d4c036cfacfdf68068ef47995c4c16848f583903
Ihor and Olha Samartsev`s <isamarcev1994@gmail.com>
2022-05-31 14:56:06 +0300

    first commit

---
+++ parse.py
@@ -0,0 +1,53 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%B3%D0%BE%D1%81%D1%83%D0%B4%D0%B0%D1%80%D1%81%D1%82%D0%B2'
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    pictures = soup.find('table').find_all('span', class_='flagicon')
+    global items; items = []
+    for item in pictures:
+        items.append({
+            'country': item.find('a', class_='image').get('title'),
+            'full_country_name': item.find_parent().find_next_sibling().find_next_sibling().get_text().rstrip(),
+            'flag_url': item.find('img', class_='thumbborder').get('src'),
+            'same_letter_count': int,
+        })
+    letter_count = []
+    for item in items:
+        for keys, values in item.items():
+            if keys == 'country':
+                letter_count.append(values[0])
+    for item in items:
+        item['same_letter_count'] = letter_count.count(item['country'][0])
+    print(items)
+    return items
+
+def parse():
+    html = get_html(url)
+    if html.status_code == 200:
+        return get_content(html.text)
+    else:
+        print("error")
+
+
+parse()
+
+
+def find_country(items):
+    x = input('Введите короткое название страны: ')
+    for item in items:
+        if x.title() == item['country']:
+            print(item)
+    print("Можете ввести еще одну страну")
+    return find_country(items)
+
+
+find_country(items)
